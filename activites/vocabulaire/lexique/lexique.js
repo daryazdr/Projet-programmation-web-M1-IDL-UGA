a03f84612c80be1d3aad7edafc8e21dae95c7694
Jiahong Zhang <jiahongzhang@JiahongdeMacBook-Air.local>
2025-12-19 01:00:52 +0800

    Intégration de l’activité lexique dans vocabulaire

---
+++ activites/vocabulaire/lexique/lexique.js
@@ -0,0 +1,53 @@
+// 让所有小图可拖动
+const draggableImages = document.querySelectorAll(".draggable-img");
+
+draggableImages.forEach(img => {
+  img.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("word", img.dataset.word);
+    e.dataTransfer.setData("imgId", img.id);
+  });
+});
+// 所有右侧框（dropzone）
+// 所有可以接收拖拽的框：右侧 dropzone + 左侧 bg-target
+const dropzones = document.querySelectorAll(".dropzone, .bg-target");
+
+
+// 允许拖东西进入框里
+dropzones.forEach(zone => {
+  zone.addEventListener("dragover", (e) => {
+    e.preventDefault();   // 必须有，不然不能 drop
+    zone.style.background = "rgba(180, 220, 255, 0.4)";  
+  });
+
+  zone.addEventListener("dragleave", () => {
+    zone.style.background = "";  // 离开时恢复样式
+  });
+
+  zone.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    // 如果这个框里已经有图，不允许再放新的
+    if (zone.querySelector("img")) {
+      return;
+    }
+
+    const word = e.dataTransfer.getData("word");
+    const imgId = e.dataTransfer.getData("imgId");
+    const img = document.getElementById(imgId);
+
+    zone.appendChild(img);
+
+    img.style.width = "100%";
+    img.style.height = "100%";
+    img.style.objectFit = "contain";
+
+    zone.classList.remove("correct", "wrong");
+    if (zone.dataset.word === word) {
+        zone.classList.add("correct");
+    } else {
+        zone.classList.add("wrong");
+    }
+  });
+
+
+});
